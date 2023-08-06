bcbb25e15d4fb2a2e077b5913c8c0e884350f6c5
lbeliakov <lbeliakov@techmategroup.com>
2023-08-06 17:08:49 +0300

    first

--- README.md
+++ README.md
@@ -3,4 +3,20 @@
 # Autogenerate PB files:
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    lavaPB/lava.proto
+    osmosisTest/osmosis.proto
+
+# Test Server with grpcurl
+
+grpcurl -plaintext localhost:3333 list
+
+grpcurl -plaintext localhost:3333 cosmos.base.tendermint.v1beta1.Service.GetLatestBlock
+
+grpcurl -plaintext -d '{"height": 10308545}' localhost:3333 cosmos.base.tendermint.v1beta1.Service.GetBlockByHeight
+
+# Test server's endpoint with grpcurl
+
+grpcurl -plaintext grpc.osmosis.zone:9090 list
+
+grpcurl -plaintext grpc.osmosis.zone:9090 cosmos.base.tendermint.v1beta1.Service.GetLatestBlock
+
+grpcurl -plaintext -d '{"height": 10308545}' grpc.osmosis.zone:9090 cosmos.base.tendermint.v1beta1.Service.GetBlockByHeight 
---
+++ server/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+
+	// "time"
+
+	pbOsmosis "github.com/BelLeonid/lavaPB/osmosisTest"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	osmosisAddress = "grpc.osmosis.zone:9090"
+	osmosisClient = CreateClient(osmosisAddress)
+)
+
+func CreateClient(address string) (client pbOsmosis.ServiceClient)  {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	log.Println("Established connection")
+	client = pbOsmosis.NewServiceClient(conn)
+	log.Println("Established new client")
+	return
+}
+
+type myGRPCServer struct {
+	pbOsmosis.UnimplementedServiceServer
+}
+
+func (m *myGRPCServer) GetLatestBlock(ctx context.Context, request *pbOsmosis.GetRequest) (*pbOsmosis.GetLatestBlockResponse, error) {
+	// Cancel request if we have no response for 15 seconds - I tested, it should be enough for server to send all the data
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 15 )
+	defer cancel()
+	osmosisResponse, err := osmosisClient.GetLatestBlock(ctx, request) // works
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+	return osmosisResponse, err
+}
+
+func (m *myGRPCServer) GetLatestValidatorSet(ctx context.Context, request *pbOsmosis.GetRequest) (*pbOsmosis.GetLatestValidatorSetResponse, error) {
+	// Cancel request if we have no response for 15 seconds - I tested, it should be enough for server to send all the data
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 15 )
+	defer cancel()
+	osmosisResponse, err := osmosisClient.GetLatestValidatorSet(ctx, request) // works
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+	return osmosisResponse, err
+}
+
+func (m *myGRPCServer) GetNodeInfo(ctx context.Context, request *pbOsmosis.GetRequest) (*pbOsmosis.GetNodeInfoResponse, error) {
+	// Cancel request if we have no response for 15 seconds - I tested, it should be enough for server to send all the data
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 15 )
+	defer cancel()
+	osmosisResponse, err := osmosisClient.GetNodeInfo(ctx, request) // works
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+	return osmosisResponse, err
+}
+
+func (m *myGRPCServer) GetSyncing(ctx context.Context, request *pbOsmosis.GetRequest) (*pbOsmosis.GetSyncingResponse, error) {
+	// Cancel request if we have no response for 15 seconds - I tested, it should be enough for server to send all the data
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 15 )
+	defer cancel()
+	osmosisResponse, err := osmosisClient.GetSyncing(ctx, request) // works
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+	return osmosisResponse, err
+}
+
+func (m *myGRPCServer) GetBlockByHeight(ctx context.Context, request *pbOsmosis.GetHeightRequest) (*pbOsmosis.GetBlockByHeightResponse, error) {
+	// Cancel request if we have no response for 15 seconds - I tested, it should be enough for server to send all the data
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 15 )
+	defer cancel()
+	osmosisResponse, err := osmosisClient.GetBlockByHeight(ctx, request) // works
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return nil, err
+	}
+	return osmosisResponse, err
+}
+
+func (m *myGRPCServer) GetValidatorSetByHeight(ctx context.Context, request *pbOsmosis.GetHeightRequest) (*pbOsmosis.GetValidatorSetByHeightResponse, error) {
+	// Cancel request if we have no response for 15 seconds - I tested, it should be enough for server to send all the data
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 15 )
+	defer cancel()
+	osmosisResponse, err := osmosisClient.GetValidatorSetByHeight(ctx, request) // works
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return nil, err
+	}
+	return osmosisResponse, err
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3333")
+	if err != nil {
+	   log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	// I added reflection so it's possible to get a list of the available services
+	reflection.Register(server)
+	myServiceServer := &myGRPCServer{}
+	pbOsmosis.RegisterServiceServer(server, myServiceServer)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := server.Serve(lis); err != nil {
+	   log.Fatalf("failed to serve: %v", err)
+	}
+ }
